496a015f838820e31eacee136e3b815d85e8ae1a
yangxiao <tfnick@qq.com>
2020-08-30 17:19:02 +0800

    完善test示例

--- yue-library-samples/yue-library-test/src/main/java/ai/yue/library/test/aspect/HttpAspect.java
+++ yue-library-samples/yue-library-test/src/main/java/ai/yue/library/test/aspect/HttpAspect.java
@@ -9,8 +9,6 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import ai.yue.library.data.redis.client.Redis;
 import ai.yue.library.web.util.servlet.ServletUtils;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,9 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Aspect
 @Component
 public class HttpAspect {
-	
-	@Autowired
-	Redis redis;
+
 	@Autowired
 	HttpServletRequest request;
 	
